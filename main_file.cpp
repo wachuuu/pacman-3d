8f603ef769ec901cdba8b807149b6d4fa48fc45b
wachuuu <gab.wachowski15@gmail.com>
2021-06-15 00:31:58 +0200

    added multiple light source

--- main_file.cpp
+++ main_file.cpp
@@ -15,6 +15,14 @@ Creeper creeper;			// Obiekt głównej postaci
 Coin coin;
 
 
+// współrzędne światła
+std::vector<glm::vec3> lightPos = {
+	glm::vec3(5.0f, 15.0f, -10.0f),
+	glm::vec3(25.0f, 20.0f, 13.0f),
+	glm::vec3(-15.0f, 20.0f, 13.0f),
+	glm::vec3(5.0f, 20.0f, 35.0f)
+};
+
 float window_width = 1280;
 float window_height = 720;
 
@@ -73,12 +81,20 @@ void freeOpenGLProgram(GLFWwindow* window) {
 	glDeleteTextures(1, &wallSegment.texture);
 }
 
+bool winCheck() //sprawdza czy zebrano wszystkie monety
+{
+	for (int i = 0; i < map_height; i++)
+		for (int j = 0; j < map_width; j++)
+			if (map[i][j] == 0)
+				return false;
+	return true;
+}
+
 void move() {
 	creeper.position = glm::scale(creeper.position, glm::vec3(4.0f, 4.0f, 4.0f));
 	int licznik = 0;
 
-	std::cout << "x: " << creeper.realPositionX << " z: " << creeper.realPositionZ << std::endl;
-	//std::cout << creeper.direction << std::endl;
+	//std::cout << "x: " << creeper.realPositionX << " z: " << creeper.realPositionZ << std::endl;
 	if (creeper.realPositionX % creeper.movingSpeed == 0)
 		creeper.arrayPositionX = creeper.realPositionX / creeper.movingSpeed;
 	if (creeper.realPositionZ % creeper.movingSpeed == 0)
@@ -120,7 +136,7 @@ void move() {
 				creeper.direction = "down";
 			}
 		}
-		std::cout << "-------------------------------\n" + creeper.direction << std::endl;
+		//std::cout << "-------------------------------\n" + creeper.direction << std::endl;
 	}
 
 	// właściwy ruch modelu
@@ -167,15 +183,25 @@ void move() {
 
 void drawScene(GLFWwindow* window) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glm::vec4 lightPosition1 = glm::vec4(lightPos[0], 1.f);
+	glm::vec4 lightPosition2 = glm::vec4(lightPos[1], 1.f);
+	glm::vec4 lightPosition3 = glm::vec4(lightPos[2], 1.f);
+	glm::vec4 lightPosition4 = glm::vec4(lightPos[3], 1.f);
+
 	glm::mat4 V = glm::lookAt(glm::vec3(5.0f, 15.0f, -1.0f), glm::vec3(5.0f, 0.0f, 5.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	glm::mat4 P = glm::perspective(glm::radians(50.0f), window_width / window_height, 1.0f, 50.0f);
-
 
 	move();
 
 	spLambertTextured->use();
 	glUniformMatrix4fv(spLambertTextured->u("P"), 1, false, glm::value_ptr(P));
 	glUniformMatrix4fv(spLambertTextured->u("V"), 1, false, glm::value_ptr(V));
+
+	glUniform4fv(spLambertTextured->u("lpos1"), 1, glm::value_ptr(lightPosition1));
+	glUniform4fv(spLambertTextured->u("lpos2"), 1, glm::value_ptr(lightPosition2));
+	glUniform4fv(spLambertTextured->u("lpos3"), 1, glm::value_ptr(lightPosition3));
+	glUniform4fv(spLambertTextured->u("lpos4"), 1, glm::value_ptr(lightPosition4));
 
 	wallSegment.drawMap(map);
 	creeper.drawModel();
@@ -217,11 +243,15 @@ int main(void)
 
 	initOpenGLProgram(window); 
 
-	//Główna pętla
 	while (!glfwWindowShouldClose(window)) {
 		glfwSetTime(0); 
 		drawScene(window); 
 		glfwPollEvents();
+		if (winCheck())
+		{
+			std::cout << "Wygrales! \n \"Lata rozpadaja sie na miesiace, miesiace na dni, dni na godziny, minuty na sekundy, a sekundy przeciekaja. \n Czy jestem? Jestem pewna iloscia sekund- ktore przeciekly.\n Rezultat: nic. Nic. \"" << std::endl;
+			break;
+		}
 	}
 
 	freeOpenGLProgram(window);
--- v_lamberttextured.glsl
+++ v_lamberttextured.glsl
@@ -4,9 +4,10 @@
 uniform mat4 P;
 uniform mat4 V;
 uniform mat4 M;
-
-
-uniform vec4 lightDir=vec4(0,0,1,0);
+uniform vec4 lpos1;
+uniform vec4 lpos2;
+uniform vec4 lpos3;
+uniform vec4 lpos4;
 
 //Atrybuty
 layout (location=0) in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
@@ -15,15 +16,26 @@ layout (location=2) in vec2 texCoord; //wspó³rzêdne teksturowania
 
 
 //Zmienne interpolowane
-out vec2 i_tc;
-out float i_nl;
+out vec4 n;
+out vec4 v;
+out vec4 l1;
+out vec4 l2;
+out vec4 l3;
+out vec4 l4;
+out vec2 iTexCoord0;
+
 
 void main(void) {
+
+    l1 = normalize(V*lpos1 - V*M*vertex);
+    l2 = normalize(V*lpos2 - V*M*vertex);
+    l3 = normalize(V*lpos3 - V*M*vertex);
+    l4 = normalize(V*lpos4 - V*M*vertex);
+    
+    n = normalize(V*M*normal);
+    v = normalize(vec4(0,0,0,1)-V*M*vertex);
+
+    iTexCoord0=texCoord;
+
     gl_Position=P*V*M*vertex;
-
-    mat4 G=mat4(inverse(transpose(mat3(M))));
-    vec4 n=normalize(V*G*normal);
-
-    i_nl=clamp(dot(n,lightDir),0,1);
-    i_tc=texCoord;
 }
